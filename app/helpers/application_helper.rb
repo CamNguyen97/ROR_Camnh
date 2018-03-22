fbedf9b10120a5f0ca9393cdeedc256b62eb82e0
45454 <hongcam941997@gmail.com>
2018-03-22 22:14:14 +0700

    Create controller not using gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title ptitle = ""
+    if ptitle.empty?
+      I18n.t("base_title")
+    else
+      ptitle + " | " + I18n.t("base_title")
+    end
+  end
+
 end
