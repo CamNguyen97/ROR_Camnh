da180e704945fcc6843460f4c84876f41aab4385
45454 <hongcam941997@gmail.com>
2018-03-22 22:14:14 +0700

    Create controller not using gem

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,13 @@ module App01
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    config.generators do |g|
+      g.template_engine false
+      g.test_framework false
+      g.assets false
+      g.helper false
+    #  g.orm :active_record, migration: false
+    end
   end
 end
