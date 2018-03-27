0bb06ea3e19e300bec47333ad227db748c26a892
45454 <hongcam941997@gmail.com>
2018-03-27 17:00:10 +0700

    signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+
     user = User.find_by email: params[:session][:email].downcase
 
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate params[:session][:password]
       log_in user
       redirect_to user
     else
